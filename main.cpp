689a267c32e7a1fae6077df7c21695ab76c56b89
awoukeng morgan <awoukengdeffomorgan@gmail.com>
2022-12-07 13:58:06 +0100

    after merge

--- main.cpp
+++ main.cpp
@@ -11,9 +11,11 @@ int main(int argc, char const *argv[])
 	std::vector<std::string> liste;
 	Auteur a("7", "Ronaldo", "Cristiano", d);
 	Livre L("Real Madrid" , a , "Espagnol" , "Football" ,d, "7777777" ,liste);
+	read_livre(L);
 	L.update_liste("Benzema");
 	L.update_liste("Benzema");
 	L.update_liste("Asensio");
 	L.update_liste("Isco");
+	read_livre(L);
 	return 0;
 }
