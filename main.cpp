0c178be43710613dcb4da4c2012f74440383b065
awoukeng morgan <awoukengdeffomorgan@gmail.com>
2022-12-07 15:10:56 +0100

    mise a jour de emprunt

--- main.cpp
+++ main.cpp
@@ -10,14 +10,28 @@ int main(int argc, char const *argv[])
     std::cout << d1;
 
 	std::vector<std::string> liste;
-	std::vector<std::string> liste_Lec;
+	std::vector<std::string> liste_Lec1;
+	std::vector<std::string> liste_Lec2;
+
 	Auteur a("7", "Ronaldo", "Cristiano", d);
-	Lecteur Lec("7", "Rayane", "Mouazer", liste_Lec);
+
+	Lecteur Lec1("7", "Rayane", "Mouazer", liste_Lec1);
+	Lecteur Lec2("723", "Christopher", "Babana", liste_Lec2);
+
 	Livre L("Real Madrid" , a , "Espagnol" , "Football" ,d, "7777777" ,liste);
+
 	read_livre(L);
+
 	L.update_liste("Isco");
-	Emprunt E(d1, L, Lec);
+	Emprunt E1(d1, L, Lec1);
+	Emprunt E2(d1, L, Lec2);
+
 	read_livre(L);
-	read_lecteur(Lec);
+
+	read_lecteur(Lec1);
+	read_lecteur(Lec2);
+	
+
+
 	return 0;
 }
